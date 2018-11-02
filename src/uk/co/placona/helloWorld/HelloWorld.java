cd8bdee44b955cffdcb9285bfbd5a1289d223c4c
ENeumann <ENeumann@FGP-FRA-NB009.ftg.local>
2018-11-02 15:54:59 +0100

    Laaaaangeweile

--- src/uk/co/placona/helloWorld/HelloWorld.java
+++ src/uk/co/placona/helloWorld/HelloWorld.java
@@ -1,9 +1,9 @@
 package uk.co.placona.helloWorld;
 
-public class HelloWorld {
-
-	public String sayHello() {
-		return "Hello World1";
+public class HelloWorld
+{
+	public String sayHello()
+	{
+		return "Hello World";
 	}
-
 }
